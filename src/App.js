ee360de86c5242285607a385424a5424b78b173a
Nazmul Hassan <hnazmul748@gmail.com>
2023-03-23 10:07:00 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './component/countries/Country';
 
+
+// main app
 function App() {
+ 
+  // const array = ["kabila","nur","hassan","rose","kibriya"];
+  // const array2 = [
+  //           {name:"khanna", job:"swiper"},
+  //           {name:"mousumi", job:"it"},
+  //           {name:"jalak", job:"marketing"},
+  //           {name:"kanika", job:"steel"},
+  //      ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
+
 
 export default App;
